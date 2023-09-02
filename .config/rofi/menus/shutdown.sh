c4564f37f438a691662e866364ae4fb56c17ca3b
gravegrow <gravegrow@gmail.com>
2023-09-02 13:40:16 +0300

    rofi updates

--- .config/rofi/menus/shutdown.sh
+++ .config/rofi/menus/shutdown.sh
@@ -7,8 +7,8 @@ entry3="  SHUTDOWN"
 chosen=$(printf "$entry1\n$entry2\n$entry3" | rofi -dmenu -i -theme-str '@import "shutdown.rasi"')
 
 case "$chosen" in
-	$entry1) pkill -u $USER;;
-	$entry2) reboot ;;
-	$entry3) poweroff ;;
+	$entry1) pkill -u $USER ;;
+	$entry2) systemctl reboot ;;
+	$entry3) systemctl poweroff ;;
 	*) exit 1 ;;
 esac
