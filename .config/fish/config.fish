0e1f00c76eabbf27c83c08ae858cc9d3384bfeb3
gravegrow <gravegrow@gmail.com>
2024-01-02 14:20:58 +0300

    storing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,12 +38,6 @@ set --export PYTHONPATH $PYTHONPATH /media/storage/dev/maya/toolset/
 set --export PYTHONPATH $PYTHONPATH /media/storage/development/maya/MayaKit/
 set --export TMPDIR /tmp
 
-set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
---border --reverse"
-
 fish_ssh_agent
 
 fish_add_path /home/gravegrow/.spicetify
@@ -58,6 +52,4 @@ end
 
 starship init fish | source
 enable_transience
-
-__tmux_file_runner
 
