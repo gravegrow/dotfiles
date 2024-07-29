a034b161b7905909118c696235cdfa4ec2eec296
gravegrow <gravegrow@gmail.com>
2024-07-29 18:22:51 +0300

    update

--- .config/nvim/lua/user/plugins/mini/init.lua
+++ .config/nvim/lua/user/plugins/mini/init.lua
@@ -4,6 +4,7 @@ return {
 		config = function()
 			require('mini.ai').setup()
 			require('mini.align').setup()
+			require('mini.icons').setup()
 			require('mini.splitjoin').setup()
 			require('mini.surround').setup()
 
