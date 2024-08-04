8684d964599c7ac6dbe87a65f779fece72d9a3eb
gravegrow <gravegrow@gmail.com>
2024-08-05 00:53:40 +0300

    fish tweaks

--- files/dotfiles/fish/config.fish
+++ files/dotfiles/fish/config.fish
@@ -1,21 +1,20 @@
-alias ls='exa -l --icons --group-directories-first'
-alias la='exa -la --icons --group-directories-first'
+alias ls='exa --icons --group-directories-first --color=always'
+alias la='exa -all  --icons --group-directories-first --color=always'
 alias tree="tree -C"
 alias py='python3'
 alias lg='lazygit'
 
 if type -q bat
-	alias cat='bat'
+    alias cat='bat'
 end
-
-set -U fish_color_autosuggestion '#555555'
 
 set --export EDITOR 'nvim'
 set --export GOPATH $HOME/.local/src/go/bin/
 set --export PATH $PATH $GOPATH
 set --export PATH $PATH ~/.local/bin/
 set --export PATH $PATH ~/.cargo/bin/
-set --export TMPDIR /tmp
+set -U FZF_COMPLETE 2
+
 
 fish_vi_key_bindings
 fish_ssh_agent
@@ -24,3 +23,4 @@ pyenv init - | source
 
 starship init fish | source
 enable_transience
+
