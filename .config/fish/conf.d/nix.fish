c9b01143df1b94847fa34e1d356330f6c8339f0c
gravegrow <gravegrow@gmail.com>
2024-08-07 00:44:12 +0300

    clean after move

--- .config/fish/conf.d/nix.fish
+++
@@ -1,2 +0,0 @@
-
-if test -e $HOME/.nix-profile/etc/profile.d/nix.fish; . $HOME/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
--- .config/fish/functions/mayalog.fish
+++
@@ -1,3 +0,0 @@
-function mayalog
-    /bin/python3 /media/storage/development/maya/maya-sender.nvim/rplugin/python3/logger.py
-end
