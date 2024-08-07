133fe5cebcce9b6fa0701fe95db3626600714902
gravegrow <gravegrow@gmail.com>
2024-08-07 14:07:35 +0300

    fish fzf config

--- files/dotfiles/fish/conf.d/fzf-opts.fish
+++ files/dotfiles/fish/conf.d/fzf-opts.fish
@@ -1,9 +1,14 @@
-set -gx FZF_DEFAULT_OPTS "\
---prompt ' ' --pointer ➤ \
---border sharp --preview-window sharp \
---height=~100% --min-height=15 --reverse --cycle --info=right \
---color=gutter:-1,pointer:1,hl+:1,hl:1,info:3,\
-fg+:#b6b2af,bg+:#25211E,border:#544C45,\
-preview-bg:0,preview-border:0 \
---bind tab:down,shift-tab:up \
-"
+set -l FZF_ICONS --prompt '" "'  --pointer '➤'  --scrollbar '┃'
+set -l FZF_BORDERS --border 'none' --preview-window 'sharp' --margin 1
+set -l FZF_HEIGHT --height '~99% '--min-height 15
+set -l FZF_LAYOUT --reverse --cycle --info=inline-right
+set -l FZF_COLORS --color 'gutter:-1,pointer:1,scrollbar:4,hl+:1,hl:1,info:3' \
+                  --color 'fg+:#b6b2af,bg+:#25211E,border:#544C45' \
+                  --color 'preview-bg:0,preview-border:0'
+set -l FZF_BINDS --bind 'tab:down,shift-tab:up,ctrl-y:accept'
+
+
+set -gx FZF_DEFAULT_OPTS $FZF_ICONS $FZF_BORDERS $FZF_HEIGHT $FZF_LAYOUT $FZF_COLORS $FZF_BINDS
+
+set -U FZF_COMPLETE 2
+set -U FZF_COMPLETE_OPTS --exact --tiebreak=begin --select-1 --info=inline-right --margin=0,1,0,1
--- files/dotfiles/fish/config.fish
+++ files/dotfiles/fish/config.fish
@@ -1,14 +1,12 @@
 alias ls='exa --icons --group-directories-first --color=always'
-alias la='exa -all  --icons --group-directories-first --color=always'
-alias tree="tree -C"
+alias la='exa --all  --icons --group-directories-first --color=always'
+alias tree='tree -C'
 alias py='python3'
 alias lg='lazygit'
 
 if type -q bat
     alias cat='bat'
 end
-
-set -U FZF_COMPLETE 2
 
 fish_vi_key_bindings
 fish_ssh_agent
