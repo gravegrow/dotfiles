4069820175cd707b30e75f4fb84dd6ed1c01e0b5
gravegrow <gravegrow@gmail.com>
2026-03-29 22:58:51 +0300

    update

--- files/dotfiles/fish/conf.d/fzf-opts.fish
+++ files/dotfiles/fish/conf.d/fzf-opts.fish
@@ -1,14 +1,15 @@
-set -l FZF_ICONS --prompt '" "' --pointer '" "' --scrollbar '┃'
-set -l FZF_BORDERS --border none --preview-window sharp,border-bold --margin 1
-set -l FZF_HEIGHT --height '~99% '--min-height 15
+set -l FZF_ICONS --prompt '"  "' --pointer '" "' --scrollbar '┃'
+set -l FZF_BORDERS --border none --preview-window sharp --margin 0,0,0,0
+set -l FZF_HEIGHT --height '~99% '
 set -l FZF_LAYOUT --reverse --cycle --info=inline-right
-set -l FZF_COLORS --color 'gutter:"#101010",pointer:2,scrollbar:4,hl+:1,hl:1,info:3' \
-    --color 'fg+:white,bg+:#1F1F22,border:gray' \
-    --color 'preview-bg:-1,preview-border:0'
-set -l FZF_BINDS --bind 'tab:down,shift-tab:up,ctrl-y:accept'
+set -l FZF_BINDS --bind 'ctrl-y:accept,ctrl-u:preview-up,ctrl-d:preview-down'
+set -l FZF_COLORS \
+    --color 'gutter:#101010,pointer:2,scrollbar:4,hl+:1,hl:1,info:#474747' \
+    --color 'fg+:white,bg+:#202020,border:gray' \
+    --color 'preview-bg:-1,preview-border:gray'
 
 
 set -gx FZF_DEFAULT_OPTS $FZF_ICONS $FZF_BORDERS $FZF_HEIGHT $FZF_LAYOUT $FZF_COLORS $FZF_BINDS
-
+ 
 set -U FZF_COMPLETE 2
-set -U FZF_COMPLETE_OPTS --tiebreak=begin --select-1 --info=inline-right --margin=0,1,0,1
+set -U FZF_COMPLETE_OPTS --select-1 --height '25%' --info=inline-right --margin 0,0,0,0
