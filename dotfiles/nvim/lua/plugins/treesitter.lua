e07bb1420849e882fdb31464e255603911edec35
gravegrow <gravegrow@gmail.com>
2026-06-28 16:43:55 +0300

    update

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,25 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    config = function()
-        local languages = {
-            "bash",
-            "lua",
-            "vim",
-            "vimdoc",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "c_sharp",
-            "ron",
-        }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        init = function()
+            vim.g.loaded_nvim_treesitter = 1
+        end,
+    },
 
-        local isnt_installed = function(lang)
-            return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
-        end
-
-        local to_install = vim.tbl_filter(isnt_installed, languages)
-        if #to_install > 0 then
-            require("nvim-treesitter").install(to_install)
-        end
-        local filetypes = {}
-        for _, lang in ipairs(languages) do
-            for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-                table.insert(filetypes, ft)
-            end
-        end
-
-        local ts_start = function(ev)
-            vim.treesitter.start(ev.buf)
-        end
-
-        vim.api.nvim_create_autocmd({ "FileType" }, {
-            group = vim.api.nvim_create_augroup("treesitter-group", { clear = true }),
-            pattern = filetypes,
-            callback = ts_start,
-            desc = "Start treesitter",
-        })
-    end,
+    {
+        "lewis6991/ts-install.nvim",
+        -- OPTIONAL
+        config = function()
+            require("ts-install").setup({
+                ensure_install = {
+                    "lua",
+                    "c",
+                    "bash",
+                    -- etc
+                },
+                auto_install = true,
+            })
+        end,
+    },
 }
