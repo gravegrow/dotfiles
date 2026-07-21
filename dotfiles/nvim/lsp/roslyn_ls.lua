8ede97c54682517d48631a12e5bac6d1817f0732
gravegrow <gravegrow@gmail.com>
2026-07-21 18:49:47 +0300

    update

--- dotfiles/nvim/lsp/roslyn_ls.lua
+++ dotfiles/nvim/lsp/roslyn_ls.lua
@@ -1,10 +1,15 @@
-return {
-    ["csharp|code_lens"] = {
-        dotnet_enable_references_code_lens = false,
-    },
-    ["csharp|background_analysis"] = {
-        dotnet_analyzer_diagnostics_scope = "openFiles",
-        dotnet_compiler_diagnostics_scope = "openFiles",
+-- for some reason just exporting config table doesnt work
+
+vim.lsp.config("roslyn_ls", {
+    settings = {
+        ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = false,
+            dotnet_enable_tests_code_lens = false,
+        },
+        ["csharp|background_analysis"] = {
+            dotnet_analyzer_diagnostics_scope = "openFiles",
+            dotnet_compiler_diagnostics_scope = "openFiles",
+        },
     },
     capabilities = {
         workspace = {
@@ -13,4 +18,6 @@ return {
             },
         },
     },
-}
+})
+
+return {}
